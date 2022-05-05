85cad5c296ac9b1d3318b892ea0b97a88e92f32c
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-05-05 09:51:18 +0600

    Initial commit

---
+++ UserInput.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class UserInput {
+    
+   
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello! Mr. "+name);
+    }
+        
+
+}
